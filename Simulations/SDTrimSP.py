26167481b3811c991a53229446ead9a3e661cdef
osang <e11805847@student.tuwien.ac.at>
2024-07-31 13:47:16 +0200

    Versuch Github zu verstehen

--- Simulations/SDTrimSP.py
+++ Simulations/SDTrimSP.py
@@ -5323,7 +5323,7 @@ class SimulationAnalysis(SimulationsAnalysis):
         return (data, plot_labels), mpl_settings
 
     def plotSurfaceLevel(self) -> Optional[Tuple[Tuple[list, list], MplCanvasSettings]]:
-        """Plot and return surface levels"""
+        """ was ist looos Plot and return surface levels"""
 
         e031_data = self.getE031TargetData()
 
